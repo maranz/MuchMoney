8e4e4fc9669818dee7623c5afdffc2d828ea78e9
maranz <luca.marani.78@gmail.com>
2014-09-22 23:34:22 +0200

    add change project into visMoney

--- js/pgVisMoney.js
+++ js/pgVisMoney.js
@@ -1,7 +1,7 @@
 pgVisMoney = {
 	load: function (  ){		
 		year = helperDate.getYear(); 
-		pgVisMoney.loadData( year );
+		pgVisMoney.loadData( year, 'all' );
 		var page = $( "#visMoney" );
 		var li = $( page ).find ( "[mz-data-list='years']" );
 		if (li.length > 0){
@@ -11,21 +11,36 @@ pgVisMoney = {
 				pgVisMoney.changeYear( year );
 			});
 		}
+		li = $( page ).find ( "[mz-data-list='projects']" );
+		if (li.length > 0){
+			var uiProject = li[0];
+			$( uiProject ).bind( "change", function( event ) {
+				var project = uiProjects.selectedItem( this );
+				pgVisMoney.changeProject( project );
+			});
+		}
 	},	
-	loadData: function ( year ){
+	loadData: function ( year, projectid ){
 			var data = {
 				 "action":"vismoney",
 				 "year":"" + year + "",
-				 "projectid":"all"
+				 "projectid":"" + projectid + ""
 	    	};			
 			helpAjax.call(data, function ( data ) {
-				if (!helperMessage.showMessageErrorJSON ( data )){
-					var $l = $("[mz-data-view='vismoney']"); 
+				var $l = $("[mz-data-view='vismoney']");
+				if (!helperMessage.showMessageErrorJSON ( data )){					
 					$.each( $l , function(index, state) {						
 						pgVisMoney.loadUI( $l[index], data );
 					});			
+				}else{
+					$.each( $l , function(index, state) {						
+						pgVisMoney.cleaner( $l[index] );
+					});
 				}		
 	   	 	});
+	},
+	cleaner: function ( ui ) {
+		$( ui ).html( "" );
 	},
 	loadUI: function (u, data){
 		var html = "";
@@ -38,9 +53,9 @@ pgVisMoney = {
 			var userid = data[i][3];
 			var username = data[i][4];
 			var avatar = data[i][5];
-			var moneyIn =  pgVisMoney.formatMoney( data[i][6] );
+			var moneyIn = pgVisMoney.formatMoney( data[i][6] );
 			var moneyOut =  pgVisMoney.formatMoney( data[i][7] );
-			var moneyDelta =  pgVisMoney.formatMoney( data[i][8] );
+			var moneyDelta = pgVisMoney.formatMoney( data[i][8] );
 			var pMoneyIn = data[i][9];
 			var pMoneyOut = data[i][10];
 			
@@ -61,24 +76,34 @@ pgVisMoney = {
 			html += "<div class='ui-money-content-right'>";
 			
 			html += "<div style='float:right;'>";
-			html += "<p>&nbsp;{0}&nbsp;-</p>".replace("{0}", moneyIn); 
+			if ( moneyIn != null){
+				html += "<p>&nbsp;{0}&nbsp;-</p>".replace("{0}", moneyIn);	
+			}
 			html += "<p>&nbsp;{0}&nbsp;</p>".replace("{0}", moneyOut);
-			html += "<p><span class='ui-span-money-result'>&nbsp;&nbsp;{0}&nbsp;</span></p>".replace("{0}", moneyDelta);
+			if ( moneyDelta != null){
+				html += "<p><span class='ui-span-money-result'>&nbsp;&nbsp;{0}&nbsp;</span></p>".replace("{0}", moneyDelta);
+			}
 			html += "</div>";
 			
-			html += "<div style='float:right;'>";				      		
-			html += "<p><span class='ui-span-money-in'>Entrate:</span></p>"; 
-			html += "<p><span class='ui-span-money-out'>Spese:</span></p>"; 
-			html += "<p>=</p>";
+			html += "<div style='float:right;'>";
+			if ( moneyIn != null){
+				html += "<p><span class='ui-span-money-in'>Entrate:</span></p>";
+			}
+			html += "<p><span class='ui-span-money-out'>Spese:</span></p>";
+			if ( moneyDelta != null){
+				html += "<p>=</p>";
+			}
 			html += "</div>";
 			
 			html += "</div>";
 			html += "</li>";
-			html += "<li class='ui-money-gra'>";			  						     	
-			html += "<div style='width:{0}%;' class='ui-div-money-in'>".replace("{0}", pMoneyIn);	
-			html += "</div>";		     		 
-			html += "<div style='width:{0}%;' class='ui-div-money-out'>".replace("{0}", pMoneyOut);
-     		html += "</div>";						
+			html += "<li class='ui-money-gra'>";	
+			if ( pMoneyIn != null){
+				html += "<div style='width:{0}%;' class='ui-div-money-in'></div>".replace("{0}", pMoneyIn);
+			}
+			if ( pMoneyOut != null){     		 
+				html += "<div style='width:{0}%;' class='ui-div-money-out'></div>".replace("{0}", pMoneyOut);
+			}					
     		html += "</li>";    		
 			
     		titlePrev = title;
@@ -90,7 +115,10 @@ pgVisMoney = {
 		$( u ).html( html );
 		$( u ).trigger("create");
 	},	
-	formatMoney: function ( text ){		
+	formatMoney: function ( text ){	
+		if ( text == null ){
+			return null;
+		}
 		var mask = new maskMoneyItem( text.length, text );
 		helperMaskEuro.maskNumText( mask );
 		return mask.text;
@@ -108,6 +136,19 @@ pgVisMoney = {
 		return html;
 	},
 	changeYear: function ( year ){
-		pgVisMoney.loadData( year );		
+		var page = $( "#visMoney" );
+		var li = $( page ).find ( "[mz-data-list='projects']" );
+		if (li.length > 0){
+			var project = uiProjects.selectedItem( li[0] );
+			pgVisMoney.loadData( year, project["id"] );
+		}
+	},
+	changeProject: function ( project ){
+		var page = $( "#visMoney" );
+		var li = $( page ).find ( "[mz-data-list='years']" );
+		if (li.length > 0){
+			var year = uiYears.selectedItem( li[0] );
+			pgVisMoney.loadData( year, project["id"]  );
+		}
 	}
 };
